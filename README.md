c38d08dc8820bd951b98367de7620cf6c7adcff1
Oleh Kutsenko <inthedark1225@gmail.com>
2022-08-16 10:38:03 +0800

    remove emotion and add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# QCT Landing
+
+## Before
+
+- install [pnpm package](https://pnpm.io/ru/installation)
+- install dependencies: `pnpm install`
+
+
+## Development
+
+- run application with: `pnpm dev`
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      jsxImportSource: "@emotion/react",
-      babel: {
-        plugins: ["@emotion/babel-plugin"],
-      },
-    }),
+    react(),
   ],
 });
